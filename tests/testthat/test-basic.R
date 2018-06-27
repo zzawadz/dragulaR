478a773f59b367b9af57e70603842471be4492b9
Zygmunt Zawadzki <zygmunt@zstat.pl>
2018-06-27 11:28:45 +0200

    add some tests

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -10,3 +10,20 @@ test_that("Create new object", {
   expect_s3_class(dragulaOutput("x"), "shiny.tag.list")
   expect_s3_class(renderDragula({dragula("x1")}), "shiny.render.function")
 })
+
+test_that("useDragulajs", {
+  expect_s3_class(useDragulajs(), "shiny.tag.list")
+})
+
+test_that("dragula value", {
+
+  output <- structure(
+    list(plots = c("mtcars", "iris", "AirPassengers", "Formaldehyde")),
+    .Names = "plots")
+  input <- structure(
+    list(`foo-plots` = list("mtcars", "iris", "AirPassengers","Formaldehyde")),
+    .Names = "foo-plots")
+
+  expect_equal(dragulaValue(input), output)
+})
+
